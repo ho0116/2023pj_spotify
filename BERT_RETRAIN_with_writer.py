dda9811e30424433014701e647e3e1ffc8c503c1
ho0116 <85285367+ho0116@users.noreply.github.com>
2023-06-21 00:54:37 +0900

    Update BERT_RETRAIN_with_writer.py
    재학습 코드

--- BERT_RETRAIN_with_writer.py
+++ BERT_RETRAIN_with_writer.py
@@ -1,4 +1,4 @@
-import pandas as pd
+import pandas as p
 import numpy as np
 from sklearn.model_selection import train_test_split
 from transformers import BertForSequenceClassification, BertTokenizer, get_linear_schedule_with_warmup, logging
